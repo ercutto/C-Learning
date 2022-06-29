2ee91d06afa38190de4a4b246297a6bc697fa9ce
ercutto <ercumentti81@gmail.com>
2022-06-29 12:02:05 +0300

    simple damageAdd project files.

---
+++ LearningC++.cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+#include<string>
+
+int main() {
+	using namespace std;
+	//Skills
+	int power= 100;
+	double mana = 5.7;
+	//healths
+	float health = 50.5f;
+	float damage = 5.0f;
+
+	
+	//booleans
+	bool isDead = false;
+
+	char character = 'e';
+
+	//Players Identities
+	string playerName = "Hanzo";
+
+
+	std::cout << "hello! "<<playerName;
+	std::cout << "\nPlayers remains health is : " << health - damage;
+
+}
