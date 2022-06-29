3572390d6e5965e676391b058544731b3b0f202d
ercutto <ercumentti81@gmail.com>
2022-06-29 13:32:20 +0300

    Making a calculation function

--- LearningC++.cpp
+++ LearningC++.cpp
@@ -1,16 +1,30 @@
 
 #include <iostream>
 #include<string>
-
+void Calculate(float a,float b) {
+	float sum = a + b;
+	float divide = a / b;
+	float multiplie = a * b;
+	float subtraction = a - b;
+	float currentSubtraction = 0;
+	std::cout << "\nbasic math + result is : " << sum;
+	std::cout << "\nbasic math / result is : " << divide;
+	std::cout << "\nbasic math * result is : " << multiplie;
+	if (subtraction < 0) {
+		float currentSubtraction = subtraction * -1;
+		std::cout << "\nbasic math - result is (but we change minus to plus) : " << currentSubtraction;
+	}
+}
 int main() {
 	using namespace std;
 	//Skills
 	int power= 100;
+	
 	double mana = 5.7;
 	//healths
 	float health = 50.5f;
 	float damage = 5.0f;
-
+	float currentHealth = health - damage;
 	
 	//booleans
 	bool isDead = false;
@@ -20,8 +34,14 @@ int main() {
 	//Players Identities
 	string playerName = "Hanzo";
 
+	//basic calculations
+	float a = 6.0f;
+	float b = 12.0f;
+	
+	std::cout << "hello! " << playerName;
+	std::cout << "\n" << playerName << "'s remains health is : " << currentHealth;
 
-	std::cout << "hello! "<<playerName;
-	std::cout <<"\n" << playerName << "'s remains health is : " << health - damage;
+	Calculate(a,b);
+	
 
 }
